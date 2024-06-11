f0fb0f33f80fc61412d77877deac44e553504b90
Dara Oyedun <oluwadara@meta.com>
2024-06-11 12:01:12 -0700

    Milestone 2 cont'd

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,48 @@ import MovieList from './MovieList.jsx';
 function App() {
     const [movies, setMovies] = useState([]);
     const [pageNumber, setPageNumber] = useState(1);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
-    const fetchURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=0cf11cf1a2cacefffa0ac6bb28bd02b2';
+    const fetchURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=abdf86b5a5bfd53f8efefa7cdcb50fa9&page=' + pageNumber.toString()+ '&language=en-US';
     fetch(fetchURL)
     .then(res => res.json())
-    .then(res => setMovies(prevMovies => prevMovies.concat(res.results)))
+    .then(res => setMovies([...movies, ...res.results]))
     .catch(err => console.error(err));}, [pageNumber]);
 
-
     function loadMorePages(){
-        setPageNumber(prevpageNumber => prevpageNumber + 1);
-        useEffect();
+        setPageNumber(prevpageNumber => prevpageNumber+1);
     }
+
+    function searchData(e){
+        e.preventDefault();
+        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=abdf86b5a5bfd53f8efefa7cdcb50fa9&page=' + pageNumber.toString()+ '&language=en-US?&query='+ search.toString() + '&language=en-US')
+        .then(res => res.json())
+        .then(data => setSearchResults([data.results]))
+        .catch(err => console.error(err));
+    }
+
+    console.log(search)
 
     return(
         <div>
             <header>
-                <h1>Flixster</h1>
-                <input type="text" placeholder='Search for a movie...' className='search-bar'/>
-                <button>Search</button>
-                <button><label>Sort by</label></button>
+                <h1 className='title-header'>Flixster</h1>
+                <input id='search-bar' type="text" className='search-bar' placeholder='Search for a movie...' value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())}/>
+                <button className='search' onClick={searchData}>Search</button>
+                <button className='sort-by'><label>Sort by</label></button>
             </header>
             <div className='MovieCard2'>
                 <MovieList data={movies}/>
             </div>
-
             <footer>
-                <p>2024 Flixster</p>
+                <div className='button'>
+                    <button onClick={loadMorePages} className='loadMoreButton'>Load More</button>
+                </div>
+                <div>
+                    <p>2024 Flixster</p>
+                </div>
             </footer>
         </div>
     )
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,16 +1,21 @@
 import './MovieList.css';
 import MovieCard from './MovieCard.jsx';
 function MovieList(props){
+    const id = []
 
     function createMovieCards(card){
-        return(
-            <MovieCard
-            movieID={card.id}
-            title={card.title}
-            img={"https://image.tmdb.org/t/p/w500" +card.poster_path}
-            rating = {card.vote_average}/>
-        )
-    }
+        if(!id.includes(card.id)){
+            id.push(card.id);
+            return(
+                <MovieCard
+                movieID={card.id}
+                title={card.title}
+                img={"https://image.tmdb.org/t/p/w500" +card.poster_path}
+                rating = {card.vote_average}/>
+            )
+        }
+        }
+
     return(
         <div className='movieCards'>
             {props.data.map(createMovieCards)}
