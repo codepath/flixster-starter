1d6fa8e992bfa2f8a0040f072d71fb59e1ef5a1d
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2024-06-13 10:48:12 -0700

    Added card animations
    Sorting and filtering does not work for some reason.

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,7 +1,6 @@
-import "./App.css"
 import Modal from "./Modal";
 import { useState, useEffect} from "react";
-// import "./MovieCard.css"
+import "./MovieCard.css"
 
 const MovieCard = (props) => {
     function handleClick () {
@@ -10,23 +9,17 @@ const MovieCard = (props) => {
     }
 
     return (
-        // <div class="card clear-fix">
-        //     <div class="cards card-movie">
-        //         <img src="https://m.media-amazon.com/images/M/MV5BOGFjYWNkMTMtMTg1ZC00Y2I4LTg0ZTYtN2ZlMzI4MGQwNzg4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" class="img-responsive" alt="Cards Image"/>
-        //         <span class="card-movie__rect"></span>
-        //         <ul class="card-movie__list">
-        //         <li>Rating: 9.5 star</li>
-        //         </ul>
-        //         <span class="card-movie__tri"></span>
-        //         <p>Godzilla King of the Monsters</p>
-        //     </div>
-        //   </div>
-        
-        <div className="card" onClick={handleClick}>
-            <h3>{props.title}</h3>
-            <img className="image" src={props.url}/>
-            <p>⭐{props.rating}</p>
-        </div>
+        <div class="card clear-fix" onClick={handleClick}>
+            <div class="cards card-movie">
+                <img src={props.url} class="img-responsive" alt="Cards Image"/>
+                <span class="card-movie__rect"></span>
+                <ul class="card-movie__list">
+                <li>Rating: ⭐{props.rating}</li>
+                </ul>
+                <span class="card-movie__tri"></span>
+                <p>{props.title}</p>
+            </div>
+          </div>
     )
-}
+}   
 export default MovieCard;
