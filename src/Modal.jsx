c9ad1e358c21a937e876b5149c00da960324413e
Maya Ody-Ajike <mayaajike@meta.com>
2024-06-13 14:18:59 -0700

    Modal Component✅

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -5,33 +5,14 @@ import { format } from 'date-fns'
 const Modal = (props) => {
     const apiKey = import.meta.env.VITE_API_KEY;
     const [genres, setGenres] = useState([]);
-    const [runtime, setRuntime] = useState('');
     const movieId = props.id;
-
-    useEffect(() => {
-        fetchMoreInfo();
-      }, [props.toggleModal])
-
-    const fetchMoreInfo = async () => {
-        const url = `https://api.themoviedb.org/3/movie/${props.id}?language=en-US&api_key=0d7613c1b95dbc61f3dd491c8f802475`
-        const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: `Bearer ${apiKey}`,
-            }
-          };
-
-          const response = await fetch(url, options);
-          const jsonResponse = await response.json();
-          setGenres(jsonResponse.genres);
-          setRuntime(jsonResponse.runtime);
-
-    }
 
     const posterImage = `https://image.tmdb.org/t/p/w500${props.image}`;
     const date = new Date(props.releaseDate);
     const formattedDate = format(date, 'MMMM d, yyyy');
+    const genreNames = props.genres.map(genre => genre.name);
+    const genreNamesString = genreNames.join(', ');
+    console.log(genreNames);
     return (
         <div className='movie-modal' style={{ display: props.isClicked ? 'block' : 'none' }}>
             <div className='modal-content'>
@@ -41,9 +22,9 @@ const Modal = (props) => {
                 <div className='modal-info'>
                     <p id="modal-title">{props.movieTitle}</p>
                     <p id='release-date'>Release Date: {formattedDate}</p>
-                    <p id="runtime">Runtime: {runtime} minutes</p>
+                    <p id="runtime">Runtime: {props.runtime} minutes</p>
                     <p id='movie-overview'>{props.movieOverview}</p>
-                    {/* <p id='movie-genre'>Genres: {genres}</p> */}
+                    <p id='movie-genre'>Genres: {genreNamesString}</p>
                 </div>
                 <span id='close-modal' onClick={props.toggleModal}>&times;</span>
             </div>
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -13,15 +13,37 @@ const MovieCard = (props) => {
         setIsClicked(!isClicked);
     }
 
+    useEffect(() => {
+        fetchMoreInfo(props.id);
+      }, [isClicked])
+
+
+
+    const fetchMoreInfo = async () => {
+        const url = `https://api.themoviedb.org/3/movie/${props.id}?language=en-US&api_key=0d7613c1b95dbc61f3dd491c8f802475`
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: `Bearer ${apiKey}`,
+            }
+          };
+
+          const response = await fetch(url, options);
+          const jsonResponse = await response.json();
+          setGenres(jsonResponse.genres);
+          setRuntime(jsonResponse.runtime);
+    }
+
     const className = Number(props.rating) < 5 ? 'bad' : Number(props.rating) < 7.5 ? 'okay' : 'good'
     return (
         <div>
-            <div className="imageContainer" onClick={toggleModal}>
+            <div className="imageContainer" onClick={toggleModal} id={props.id} >
                 <img src={props.image} id="movie-poster"/>
                 <p id="movie-title">{props.title}</p>
                 <p id="movie-rating" className={className}>{props.rating}</p>
             </div>
-            <Modal  id={props.id} isClicked={isClicked} toggleModal={toggleModal} movieTitle={props.title} image={props.image} releaseDate={props.releaseDate} movieOverview={props.movieOverview} movieGenres={props.movieGenres}/>
+            <Modal  id={props.id} isClicked={isClicked} toggleModal={toggleModal} movieTitle={props.title} image={props.image} releaseDate={props.releaseDate} movieOverview={props.movieOverview} movieGenres={props.movieGenres} runtime={runtime} genres={genres}/>
         </div>
     )
 }
